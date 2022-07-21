fd1b63b01ac0adf0829a1e14987039e932c562ab
Aniki Yusuf <anikiyusuf130@gmail.com>
2022-07-21 22:06:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var password = document.getElementById("genPassword");
+
+
+
+
+function genPassword(){
+
+var chars ="0123456789!@#$%^&*()_-+=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength =12;
+var password="";
+
+for (var i=0; i<= passwordLength ; i++){
+    var randomNumber = Math.floor(Math.random()*chars.length);
+    password += chars.substring(randomNumber ,randomNumber +1);
+
+    document.getElementById("password").value = password;
+
+}
+}
+
+function copyPassword(){
+    var copyText = document.getElementById("password");
+    copyText.select();
+    document.execCommand("copy");
+}
